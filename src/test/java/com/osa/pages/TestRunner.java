3295d8a4c4d36dd288548ceb5c914dd5e4f734c6
Habib <shohelhabib1989@gmail.com>
2021-01-05 21:35:24 -0500

    genarating test runner class

---
+++ src/test/java/com/osa/pages/TestRunner.java
@@ -0,0 +1,28 @@
+package com.osa.pages;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	features= {""},
+	glue= {""},
+	tags= {""},
+	plugin= {"pretty", "html:target/html_report"}
+		
+		
+		
+		
+		
+		
+		)
+
+public class TestRunner {
+	
+	
+	
+	
+
+}
