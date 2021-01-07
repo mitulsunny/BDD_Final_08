9c631298fc1a42f350804a74827e4a3fd518b58c
Habib <shohelhabib1989@gmail.com>
2021-01-07 17:57:17 -0500

    update browser class And connect log4j

--- src/main/java/com/osa/base/Browser.java
+++ src/main/java/com/osa/base/Browser.java
@@ -10,19 +10,31 @@ public class Browser {
 	
 	static WebDriver dr=null;
 	static String basePath=System.getProperty("user.dir");
-	
+	static String os=System.getProperty("os.name");
+	/**
+	 * this method will open web browser on different operating system
+	 * @param browser
+	 * @return webDriver
+	 */
 	public static WebDriver openBrowser(String browser) {
-		
-
-		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+		if(os.toLowerCase().contains("windows")) {
+			System.out.println("Browser is running on "+ os);
 			getBrowserForWindows(browser);
 		}else {
+			System.out.println("Browser is running on "+ os);
 			getBrowserForMac(browser);
-			
 		}
+		dr.manage().deleteAllCookies();
+		dr.manage().window().maximize();
+		
 		 
 		return dr;
 	}
+	
+	/**
+	 * this method will open Web Browser on Windows operating System.
+	 * @param browser
+	 */
 	public static void getBrowserForWindows(String browser) {
 		if(browser.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", basePath+"\\src\\test\\resources\\drivers\\geckodriver.exe");
@@ -49,6 +61,11 @@ public class Browser {
 //			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
 //		}
 //	}
+	
+	/**
+	 * this method will open Web Browser on mac operating System.
+	 * @param browser
+	 */
 	public static void getBrowserForMac(String browser) {
 		if(browser.equalsIgnoreCase("chrome")) {
 			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/drivers/mac/chromedriver");
